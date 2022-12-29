8f93c4940af40ed546aa5e29100e2ccbecdf3b17
jjfreeburn <118950347+jjfreeburn@users.noreply.github.com>
2022-12-29 14:50:25 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+const menu = {
+  courses: {
+    appetizers:[],
+    mains:[],
+    desserts:[],
+  },
+addDishToCourse: function(courseName,dishName,dishPrice){
+  dish:{
+    name:
+  }
+}
+};
+
+console.log(menu.courses);
